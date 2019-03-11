b4f9eb06576205a7a05c831bfdb907a27d3749ba
Michael Millspaugh <tk421storm@gmail.com>
2019-03-11 17:47:42 -0400

    work with task updater dialog, still havent found right way to communicate with it

--- python/tk_multi_publish2/qtwidgets.py
+++ python/tk_multi_publish2/qtwidgets.py
@@ -13,11 +13,76 @@ Wrapper for the various widgets used from frameworks so that they can be used
 easily from with Qt Designer
 """
 
+from pprint import pprint
+
+
 import sgtk
+from tank.platform.qt import QtCore, QtGui
 
 elided_label = sgtk.platform.import_framework("tk-framework-qtwidgets", "elided_label")
 ElidedLabel = elided_label.ElidedLabel
 
 context_selector = sgtk.platform.import_framework("tk-framework-qtwidgets", "context_selector")
 ContextWidget = context_selector.ContextWidget
+
+
+#Millspaugh, create a dropdown for whether to update the task status on publish creation or not
+class TaskUpdate(QtGui.QWidget):
+	
+	taskChanged=QtCore.Signal(object)
+	
+	def __init__(self, parent):
+		
+		QtGui.QWidget.__init__(self)
+		
+		self.taskLayout=QtGui.QGridLayout()
+
+		self.taskLayout.setObjectName("taskLayout")
+		self.updateTaskCheckbox=QtGui.QCheckBox()
+		self.updateTaskCheckbox.setChecked(True)
+		self.taskLayout.addWidget(self.updateTaskCheckbox, 0, 0)
+		self.taskLayout.setColumnStretch(0, 0)
+		self.taskLabel=QtGui.QLabel('Update task on publish to: ')
+		self.taskLayout.addWidget(self.taskLabel, 0, 1)
+		self.taskLayout.setColumnStretch(1, 0)
+		self.updateTaskMenu=QtGui.QComboBox()
+		model=self.updateTaskMenu.model()
+		for name in ['Pending Review', 'In Progress']:
+			item=QtGui.QStandardItem(name)
+			model.appendRow(item)
+		self.taskLayout.addWidget(self.updateTaskMenu, 0, 2)
+		self.taskLayout.setColumnStretch(2, 1)
+		self.taskLayout.setContentsMargins(0, 0, 0, 0)
+		
+		self.setLayout(self.taskLayout)
+		
+		#set active/enabled (for use after loading stored settings, if any)
+		self.enableDisable()
+		self.updateTaskCheckbox.clicked.connect(self.enableDisable)
+
+		
+	def enableDisable(self):
+		'''enable/disable dialog based on checkbox'''
+		
+		if self.updateTaskCheckbox.isChecked():
+			self.taskLabel.setEnabled(True)
+			self.updateTaskMenu.setEnabled(True)
+			self.active=True
+		else:
+			self.taskLabel.setEnabled(False)
+			self.updateTaskMenu.setEnabled(False)
+			self.active=False
+			
+		#emit that something changed
+		self.taskChanged.emit(self.updateTask())
+			
+	def updateTask(self):
+		'''return False if dont do update, otherwise string of selected update type'''
+		
+		if self.updateTaskCheckbox.isChecked():
+			return self.updateTaskMenu.currentText()
+		else:
+			return False
+		
+
 
